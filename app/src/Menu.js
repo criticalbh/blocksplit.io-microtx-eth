e506b79cdbf273a9ac413bf32db3ae92c4cbeb49
Admir Sabanovic <admir.sabanovic@moduscreate.com>
2018-04-25 13:56:29 +0200

    establish socket connection

--- app/src/Menu.js
+++ app/src/Menu.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from "react";
 
 class Menu extends Component {
+  constructor() {
+    super();
+    this.socket = io("http://localhost:8080"); // eslint-disable-line no-undef
+    this.socket.on("helloworld", function (data) {
+      console.log(data);
+    });
+  }
+
+  sendMessage() {
+    console.log("aaa");
+    this.socket.emit("buyitem", {my: "data"});
+  }
+
   render() {
     return (
-      <h1>
-        Menu
-      </h1>
+      <div>
+        <h1>
+          Menu
+        </h1>
+        <button onClick={this.sendMessage.bind(this)}>Click</button>
+      </div>
     );
   }
 }
